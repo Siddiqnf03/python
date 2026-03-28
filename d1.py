9ae42c90b5625335d85bd7e8798b32df062783bb
Siddiq N F <siddiqnf03@gmail.com>
2026-03-28 15:09:59 +0530

    first commit

---
+++ d1.py
@@ -0,0 +1,13 @@
+a = input("enter your name\n")
+b = int(input("enter your usn\n"))
+c = int(input("enter your phn no\n"))
+d = int(input("enter your age\n"))
+
+print("------student details-------")
+print("name:",a)
+print("usn:",b)
+print("phone number:",c)
+print("age:",d)
+
+
+
---
+++ jeevan.py
@@ -0,0 +1,10 @@
+a = input("enter your name\n")
+b = int(input("enter your usn\n"))
+c = int(input("enter your phone num\n"))
+d = int(input("enter your age\n"))
+
+print("-----student details-----")
+print("name:",a)
+print("usn:",b)
+print("phone num:",c)
+print("age:",d)
