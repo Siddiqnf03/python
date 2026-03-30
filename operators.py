12b79e4644c8e2524ec2229b33918853863898e1
Siddiq N F <siddiqnf03@gmail.com>
2026-03-30 15:27:16 +0530

    added work

---
+++ operators.py
@@ -0,0 +1,33 @@
+# arithmetic operator
+# a = 10
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
+
+#relational (comparision )op
+
+# a = 10
+# b = 10
+
+# print(a==b) #false
+# print(a!=b) #true
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+
+#logical
+
+# a= True
+# b= False
+# print(not b)
+
+#assignment
+
+for i in range(1,10):
+    a=i
+    print(i)
---
+++ work.py
@@ -0,0 +1,30 @@
+# a = 20
+# b = 10
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
+
+# a = 40
+# b = 10
+# print(a==b)
+# print(a!=b)
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+
+
+# for i in range(8):
+#     a=i
+#     i+=a
+#     print(i)
